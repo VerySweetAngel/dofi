56d4a1016bf296e5bef0770b39bbe6867f27c26c
tirpitz-verus <marek.lesiewski@gmail.com>
2013-09-11 21:29:18 +0200

    dostosowaie HibernateUtil do współpracy z testami Utworzenie testów dla modelu User

--- dofi/src/main/java/pl/com/setvar/dofi/dao/HibernateUtil.java
+++ dofi/src/main/java/pl/com/setvar/dofi/dao/HibernateUtil.java
@@ -12,22 +12,51 @@ import org.hibernate.cfg.Configuration;
 import pl.com.setvar.dofi.util.DefaultLogger;
 
 /**
- * Klasa obslługuje bazę danych. Zwraca fabrykę sesji i aktualną sesję. 
+ * Klasa obslługuje bazę danych. Zwraca fabrykę sesji i aktualną sesję. Obiekt klasy służy do obsługi transakcji.
  *
  * @author tirpitz
  */
 public class HibernateUtil {
+    
+    /** transakcja w ramach bazy danych */
+    private Transaction transaction;
+    
+    /** metoda rozpoczyna transakcję w ramach bazy danych */
+    public void beginTransaction(){
+        transaction = getSessionFactory().getCurrentSession().beginTransaction();
+        DefaultLogger.HIBERNATE.debug("HibernateUtil.beginTransaction transaction begun");
+    }
+    
+    /** metoda zatwierdza transakcję w ramach bazy danych */
+    public void commitTransaction(){
+        transaction.commit();
+        DefaultLogger.HIBERNATE.debug("HibernateUtil.commitTransaction transaction commited");
+    }
+    
+    /** metoda cofa transakcję, jeżeli ta istnieje */
+    public void rollbackTransaction(){
+        if (transaction != null){
+            transaction.rollback();
+            DefaultLogger.HIBERNATE.debug("HibernateUtil.rollbackTransaction transaction rollback");
+        } else {
+            DefaultLogger.HIBERNATE.debug("HibernateUtil.rollbackTransaction no transaction to rollback");
+        }
+    }
 
+    /** fabryka sesji */
     private static SessionFactory sessionFactory;
 
+    /** funkcja zwrca fabryke sesji */
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
     
+    /** funkcja zwraca aktualną sesję */
     public static Session getCurrentSession(){
         return sessionFactory.getCurrentSession();
     }
 
+    /** funkcja tworzy nową fabrykę sesji w ramach klasy */
     public static void createFactory() {
         try {
             sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -38,24 +67,22 @@ public class HibernateUtil {
         }
     }
 
+    /** funkcja służy do właczenia w filtr - każde rządanie będzie otoczone sesją i transakcją */
     public static void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         DefaultLogger.HIBERNATE.debug("HibernateUtil.doFilter filtering request");
-        Transaction transaction = null;
+        HibernateUtil hibernateUtil = new HibernateUtil();
         try {
-            transaction = sessionFactory.getCurrentSession().beginTransaction();
-            DefaultLogger.HIBERNATE.debug("HibernateUtil.doFilter transaction begun");
+            hibernateUtil.beginTransaction();
             chain.doFilter(request, response);
-            transaction.commit();
-            DefaultLogger.HIBERNATE.debug("HibernateUtil.doFilter transaction commited");
+            hibernateUtil.commitTransaction();
         } catch (Throwable ex) {
             DefaultLogger.HIBERNATE.error("HibernateUtil.doFilter session error", ex);
-            if (transaction != null) {
-                transaction.rollback();
-                DefaultLogger.HIBERNATE.error("HibernateUtil.doFilter session rollback", ex);
-            }
+            hibernateUtil.rollbackTransaction();
         }
+        DefaultLogger.HIBERNATE.debug("HibernateUtil.doFilter request filtered");
     }
 
+    /** funckaj zamyka sesję */
     public static void closeFactory(){
         sessionFactory.close();       
         DefaultLogger.HIBERNATE.info("HibernateUtil.close session factory closed");
---
+++ dofi/src/test/java/pl/com/setvar/dofi/model/UserTests.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.setvar.dofi.model;
+
+import java.util.List;
+import static org.testng.Assert.*;
+import org.testng.annotations.*;
+import pl.com.setvar.dofi.dao.UserDao;
+import pl.com.setvar.dofi.util.BaseTestWithHibernate;
+
+/**
+ *
+ * @author tirpitz
+ */
+public class UserTests extends BaseTestWithHibernate {
+    
+    /**
+     *test sprawdza, czy findAll zwróci przynajmniej dwóch userów (admin i tester)
+     */
+    @Test
+    public void testFindAll() {
+        List<User> result = User.findAll();
+        boolean adminPresent = false;
+        boolean testerPresent = false;
+        for(User user : result){
+            String login = user.getLogin();
+            boolean admin = user.isIsAdmin();
+            if(admin && login.equals("admin"))
+                adminPresent = true;
+            if(!admin & login.equals("tester"))
+                testerPresent = true;
+        }
+        assertTrue(adminPresent, "brak użytkownika aadmin");
+        assertTrue(testerPresent, "brak użytkownika tester");
+    }
+
+    /**
+     * test sprawdza, czy można utworzyć usera i go zalogować
+     */
+    @Test
+    public void saveAndLogMeIn() {
+        String login = "sdfsdf";
+        String password = "dsdwedw";
+        String email = "dsdwedw@com.pl";
+        String realName = "sdf sfdsd";
+        boolean isAdmin = false;
+        UserDao dao = new UserDao();
+        
+        User expected = new User(login, password);
+        expected.setEmail(email);
+        expected.setIsAdmin(isAdmin);
+        expected.setRealName(realName);
+        dao.save(expected);
+        
+        User actual = new User(login, password);
+        assertTrue(actual.loadIfExistsByCredentials(), "zapisany user powinen dać się zalogować");
+        assertEquals(actual.getLogin(), expected.getLogin());
+        assertEquals(actual.getPassword(), expected.getPassword());
+        assertEquals(actual.getEmail(), expected.getEmail());
+        assertEquals(actual.getRealName(), expected.getRealName());
+        assertEquals(actual.isIsAdmin(), expected.isIsAdmin());
+    }
+
+}
