2e48d62769f99e3b8e93f6c552f084db410b9ed6
tirpitz-verus <tirpitz.verus@gmail.com>
2013-09-12 14:21:52 +0200

    poprawienie testu tagów poprzez użycie namedQuery Signed-off-by:tirpitz-verus <tirpitz.verus@gmail.com>

--- dofi/src/main/java/pl/com/setvar/dofi/dao/TagDao.java
+++ dofi/src/main/java/pl/com/setvar/dofi/dao/TagDao.java
@@ -6,7 +6,6 @@ package pl.com.setvar.dofi.dao;
 
 import java.util.List;
 import pl.com.setvar.dofi.model.Tag;
-import pl.com.setvar.dofi.model.Taglink;
 
 /**
  *
@@ -15,7 +14,7 @@ import pl.com.setvar.dofi.model.Taglink;
 public class TagDao extends GenericDao {
     
     public List<Tag> findTags(String criteria){
-        return (List<Tag>) getSession().createQuery("SELECT T.tag FROM Taglink T WHERE T.word LIKE :criteria OR T.tag.tagname LIKE :criteria")
+        return (List<Tag>) getSession().getNamedQuery("findTagsByTagnameAndTaglinkWord")
                 .setString("criteria", "%" + criteria + "%")
                 .list();
     }
